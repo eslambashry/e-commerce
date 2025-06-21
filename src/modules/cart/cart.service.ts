b599efbb34c791075ad230da1eb7863bb4160364
Eslam Bashry <eslamhussin600@gmail.com>
2025-06-21 23:18:37 +0300

    fix cart path

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Cart, CartDocument } from 'src/core/schemas/cart';
+import { Cart, CartDocument } from '../../core/schemas/cart';
 
 @Injectable()
 export class CartService {
